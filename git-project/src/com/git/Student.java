0c856ed3abc99ad10be9097c19a3f4689df50666
huangbangyi2019 <1094476088@qq.com>
2020-06-23 14:40:32 +0800

    modefy student

--- git-project/src/com/git/Student.java
+++ git-project/src/com/git/Student.java
@@ -5,30 +5,22 @@ public class Student {
 	private int id;
 	private String username;
 	private String password;
-	
 	public int getId() {
-<<<<<<< HEAD
 		return id + 10;
-=======
-
-		return id + 10;
-
-
->>>>>>> branch 'master' of https://github.com/huangbangyi2019/git_project2.git
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
 	public String getUsername() {
-		return username+110;
+		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	public String getPassword() {
-		return password+102;
+		return password;
 	}
-	public void setPassword(String password)  {
+	public void setPassword(String password) {
 		this.password = password;
 	}
 }
