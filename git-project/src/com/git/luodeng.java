56c123fdc2415391e75eecb52325a6fed87aac7d
huangbangyi2019 <1094476088@qq.com>
2020-06-23 11:08:12 +0800

    luodeng

---
+++ git-project/src/com/git/luodeng.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class luodeng {
+	
+	
+	private int id;
+
+	
+	public int getId() {
+		return id + 1000000;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
